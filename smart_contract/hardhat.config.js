5ede96f90afa278cbb25c350ef67f0f3a726275b
Lavanaya <lavanayakhosla@yahoo.in>
2025-01-31 01:49:35 +0530

    Initial commit: React + Solidity project

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/Iz-1MmAqUENRCLpZtlDLHp63npzROpmh',
+      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    },
+  },
+};
